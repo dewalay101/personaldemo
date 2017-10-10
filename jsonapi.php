fc9429055052df8cf797bf3784819f5f2dd82ddb
dewalay101 <32671326+dewalay101@users.noreply.github.com>
2017-10-10 17:46:49 +0100

    Create jsonapi.php

---
+++ jsonapi.php
@@ -0,0 +1,12 @@
+<?php
+
+$items = array();
+
+for($i = 0; $i < 10; $i++) {
+ $items[$i] + "This is message $i";
+ }
+ header('Content-Type: application/json');
+ 
+    $jsonOutput = json_encod($items);
+    
+ echo $jsonOutput;   
